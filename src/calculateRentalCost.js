d536ac7a0b72d8beb967280c747e6dba7fefe500
VadimBatsko <batsko1999god@gmail.com>
2026-02-12 13:36:41 +0200

    correction

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,19 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  RENT_DAY_COST = 40;
-  FULL_DISCOUNT = 50;
-  SMALL_DISCOUNT = 20;
+  const RENT_DAY_COST = 40;
+  const FULL_DISCOUNT = 50;
+  const SMALL_DISCOUNT = 20;
 
   if (days >= 7) {
-    return days * RENT_DAY_COST - FULL_DISCOUNT
+    return days * RENT_DAY_COST - FULL_DISCOUNT;
   }
 
   if (days >= 3) {
-    return days * RENT_DAY_COST - SMALL_DISCOUNT
+    return days * RENT_DAY_COST - SMALL_DISCOUNT;
   }
 
-  return days * RENT_DAY_COST
+  return days * RENT_DAY_COST;
 }
 
 module.exports = calculateRentalCost;
