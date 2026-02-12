d0e5244a2bd5e8cd8983a39fba55e5451a913bf0
VadimBatsko <batsko1999god@gmail.com>
2026-02-12 13:03:16 +0200

    Implement rental cost calculation with discounts
    Add rental cost calculation with discounts based on days rented.

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  RENT_DAY_COST = 40;
+  FULL_DISCOUNT = 50;
+  SMALL_DISCOUNT = 20;
+
+  if (days >= 7) {
+    return days * RENT_DAY_COST - FULL_DISCOUNT
+  }
+
+  if (days >= 3) {
+    return days * RENT_DAY_COST - SMALL_DISCOUNT
+  }
+
+  return days * RENT_DAY_COST
 }
 
 module.exports = calculateRentalCost;
